e6a536b58dcf0cd1bf499eb7adb2b695a4a9533e
Ayçanur GÜÇ <99604369+aycanur25@users.noreply.github.com>
2023-06-04 14:53:01 +0300

    Add files via upload

---
+++ Görsel Programlama Projee/Expence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Expence
+{
+	public Expence()
+	{
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=DESKTOP-P816U58\\MSSQLSERVER2;Initial Catalog=project;Integrated Security=True";// Veritabanı bağlantı dizesi
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT ExpCat, SUM(ExpAmt) AS TotalExpense FROM ExpTbl GROUP BY ExpCat"; // ExpTbl, tablo adı
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string category = reader["ExpCat"].ToString();
+                    decimal totalExpense = Convert.ToDecimal(reader["TotalExpense"]);
+
+                    Console.WriteLine("Category: " + category + ", Total Expense: " + totalExpense);
+                }
+
+                reader.Close();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Görsel Programlama Projee/Income.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Income
+{
+	public Income()
+	{
+        static void Main()
+        {
+
+            string connectionString = "Data Source=DESKTOP-P816U58\\MSSQLSERVER2;Initial Catalog=project;Integrated Security=True";// Veritabanı bağlantı dizesi
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT IncCat, SUM(InAmt) AS TotalIncome FROM IncomeTbl GROUP BY IncCat"; // income, tablo adı
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string category = reader["IncCat"].ToString();
+                    decimal totalIncome = Convert.ToDecimal(reader["TotalIncome"]);
+
+                    Console.WriteLine("Category: " + category + ", Total Income: " + totalIncome);
+                }
+
+                reader.Close();
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
